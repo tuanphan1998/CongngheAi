8a2c42f08fcfac410f2d0a2e1e59c2bbda0303f0
TUanphan nam <phantuanw.o.t@gmail.com>
2020-08-22 23:01:03 +0700

    Updates

---
+++ precache-manifest.d9a1584d3fe88a38f2dbca901f764790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ea5d5ac8fa354b3778b",
+    "url": "/CongngheAi/static/css/main.ee4291fd.chunk.css"
+  },
+  {
+    "revision": "2ea5d5ac8fa354b3778b",
+    "url": "/CongngheAi/static/js/main.ef566474.chunk.js"
+  },
+  {
+    "revision": "c7863513e99bf73228b0",
+    "url": "/CongngheAi/static/js/runtime~main.09b7419b.js"
+  },
+  {
+    "revision": "272a60ec6fc73baca479",
+    "url": "/CongngheAi/static/css/2.a8b0f192.chunk.css"
+  },
+  {
+    "revision": "272a60ec6fc73baca479",
+    "url": "/CongngheAi/static/js/2.bea29f3b.chunk.js"
+  },
+  {
+    "revision": "6943aa26c113061e5da180c8878c5a69",
+    "url": "/CongngheAi/index.html"
+  }
+];
